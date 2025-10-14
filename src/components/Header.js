3b9299eb0e097f4acba5102fcb76b0c3fa937162
Supriya Patil <supriyapatil084@gmail.com>
2025-10-14 18:15:41 +0530

    SignIn/SignUp form && Login/header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img
+      className="w-44" 
+      src="logo.svg" alt="logo" />
+    </div>
+  );
+} 
+
+export default Header
